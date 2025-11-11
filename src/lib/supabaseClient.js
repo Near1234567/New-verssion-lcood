04040f8aa033d73ed38c3d43b9c4d302c47ef944
Near1234567 <111826632+Near1234567@users.noreply.github.com>
2025-11-11 10:25:57 +0100

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,12 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://myywxfguwjacuwwzrqcg.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15eXd4Zmd1d2phY3V3d3pycWNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyNDM2NjYsImV4cCI6MjA2NTgxOTY2Nn0.cuJjPV7h5AA1D4kj_rD0pmgEfeogv-p6Gtd8E-FN1Tw';
+const supabaseUrl = 'https://tymrvpxcokvbebhsmafq.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5bXJ2cHhjb2t2YmViaHNtYWZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4MTc4NTAsImV4cCI6MjA3ODM5Mzg1MH0.VvhH2P-akjfnkBHuxGEHsIQYvLTTSH6Z8Nw5almfUbw';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: true
-  }
+    detectSessionInUrl: true,
+  },
 });
